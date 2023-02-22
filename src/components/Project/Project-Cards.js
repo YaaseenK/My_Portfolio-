517d9f93abbf32f52efe2a289c7eb0fb52fe1a98
[firstname <yaaseenkhan02@gmail.com>
2023-02-22 10:10:37 -0500

    repo links to projects

--- src/components/Project/Project-Cards.js
+++ src/components/Project/Project-Cards.js
@@ -1,13 +1,13 @@
 import { Col } from "react-bootstrap";
 
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+export const ProjectCard = ({ title, description, imgUrl, reopURL}) => {
     return (
         <Col size={12} sm={6} md={4}>
             <div className="proj-imgbx">
                 <img src={imgUrl} />
                 <div className="proj-txtx">
-                <h4>{title}</h4>
+                <a href={reopURL} className="repo"><h4>{title}</h4></a>
                 <span>{description}</span>
                 </div>
             </div>
--- src/components/Project/projects-Array.js
+++ src/components/Project/projects-Array.js
@@ -11,31 +11,37 @@ export const projects = [
         title: "Horiseon Website",
         description: "HTML, CSS, and Git: Code Refactor",
         imgUrl: projImg1,
+        reopURL: "https://github.com/YaaseenK/Horiseon-Website"
     },
     {
         title: "Portfolio",
         description: "A portfolio of work can showcase my skills and talents to employers looking to fill a part-time or full-time position.",
         imgUrl: projImg2,
+        reopURL: "https://github.com/YaaseenK/Advanced-CSS-Challenge"
     },
     {
         title: "Password Generator",
         description: "This application enables employees to generate random passwords based on criteria that they’ve selected. This app will run in the browser and features dynamically updated HTML and CSS powered by JavaScript code written by me. It has a clean and polished, responsive user interface that adapts to multiple screen sizes.",
         imgUrl: projImg3,
+        reopURL: "https://github.com/YaaseenK/PasswordGenerator"
     },
     {
         title: "Work Day Scheduler",
         description: "Simple calendar application that allows a user to save events for each hour of the day. This app will run in the browser and features dynamically updated HTML and CSS powered by jQuery.",
         imgUrl: projImg4,
+        reopURL: "https://github.com/YaaseenK/Daily_Planner"
     },
     {
         title: "Meme God",
         description: "This is a responsive front end application that uses 2 Sever-side API to retrieve humourous memes from various sources for the viewers amusement. -This application uses client-side storage to store persistent data.",
         imgUrl: projImg5,
+        reopURL: "https://github.com/YaaseenK/Team-Project-1"
     },
     {
         title: "Weather Dashboard",
         description: "A weather dashboard that will run in the browser and feature dynamically updated HTML and CSS. Use the OpenWeather One Call API to retrieve weather data for cities. Read through the documentation for setup and usage instructions. You will use localStorage to store any persistent data.",
         imgUrl: projImg6,
+        reopURL: "https://github.com/YaaseenK/Weather_Dashboard"
     },
 ];
 
@@ -44,11 +50,13 @@ export const projectsTWO = [
         title: "Horiseon Website",
         description: "HTML, CSS, and Git: Code Refactor",
         imgUrl: projImg1,
+        reopURL: ""
     },
     {
         title: "Portfolio",
         description: "A portfolio of work can showcase my skills and talents to employers looking to fill a part-time or full-time position.",
         imgUrl: projImg2,
+        reopURL: ""
     },
     {
         title: "Password Generator",
